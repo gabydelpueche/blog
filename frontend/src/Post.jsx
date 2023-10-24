aac764f7a3ac045dd00d231d8da0059f7b91fcb2
Gabriella Delpueche <gdelpu720@west-mec.org>
2023-10-24 14:28:26 -0700

    sending post request

--- frontend/src/Post.jsx
+++ frontend/src/Post.jsx
@@ -4,25 +4,36 @@ import React from 'react'
 import Select from 'react-select'
 
 export default function Post() {
-    const [post, setPost] = useState({category: "", title:"", description:"", content:""})
+    const [post, setPost] = useState({ category: "", title: "", description: "", content: "" })
     const [openModal, setOpenModal] = useState();
     const props = { openModal, setOpenModal };
 
     const detectChange = (e) => {
-        console.log(e.target)
-        setPost({...post, [e.target.name]: e.target.value})
+        setPost({ ...post, [e.target.name]: e.target.value })
     };
 
-    const detectSelect = (e) =>{
-        setPost({...post, category: e.value})
+    const detectSelect = (e) => {
+        setPost({ ...post, category: e.value })
     }
 
-    const postBlog = (e) =>{
-        
-
+    const postBlog = (e) => {
         e.preventDefault();
-        window.location.href = "/home";
-        console.log(post)
+
+        console.log(post);
+
+        try {
+            fetch("http://localhost:3000/createPost", {
+                method: "POST",
+                headers: { 
+                    "Content-Type": "application/json" 
+                }, 
+                body: JSON.stringify(post) 
+            })
+                .then(res => res.json())
+                .then(data => console.log(data))
+        } catch (err) {
+            console.error(err)
+        }
     }
 
     const options = [
@@ -38,10 +49,6 @@ export default function Post() {
         resize: 'none'
     }
 
-    const redirect = (e) => {
-        
-    };
-
     return (
         <>
             <Button onClick={() => props.setOpenModal('createPost')}>Add Post</Button>
@@ -49,57 +56,57 @@ export default function Post() {
                 <Modal.Header />
                 <Modal.Body>
                     <form onSubmit={postBlog}>
-                    <div className="w-full">
-                        <h3 className=" text-center mb-2 text-xl font-medium text-gray-900 dark:text-white">Add Post</h3>
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="category" value="Category" />
-                            </div>
-                            <Select name='category' value={options.find((option) => option.value === post.category)} onChange={detectSelect} options={options}/>
-                        </div>
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="title" value="Title" />
-                            </div>
-                            <TextInput name="title" value={post.title} onChange={detectChange} className=" text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-red-600 focus:border-red-600 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-600 dark:focus:border-red-600" id="title" placeholder="Title" required />
-                        </div>
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="description" value="Description" />
-                            </div>
-                            <TextInput name="description" value={post.description} onChange={detectChange} className=" text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-red-600 focus:border-red-600 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-600 dark:focus:border-red-600" id="description" placeholder='Optional' />
-                        </div>
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="content" value="Content" />
-                            </div>
-                            <button type="button" class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
-                                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                                    <path fill="currentColor" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z" />
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z" />
-                                </svg>
-                                <span class="sr-only">Upload image</span>
-                            </button>
-                            <button type="button" class="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
-                                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.408 7.5h.01m-6.876 0h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM4.6 11a5.5 5.5 0 0 0 10.81 0H4.6Z" />
-                                </svg>
-                                <span class="sr-only">Add emoji</span>
-                            </button>
-                            <textarea name="content" value={post.content} onChange={detectChange} style={textareaStyle} rows="1" class="block mx-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-red-600 focus:border-red-600 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-600 dark:focus:border-red-600" placeholder="Your message..."></textarea>
-                        </div>
-                        <div>
-                            <button type='submit' className="w-full mt-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-                                <div className="flex items-center justify-center">
-                                    Post
-                                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                    </svg>
+                        <div className="w-full">
+                            <h3 className=" text-center mb-2 text-xl font-medium text-gray-900 dark:text-white">Add Post</h3>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="category" value="Category" />
                                 </div>
-                            </button>
+                                <Select name='category' value={options.find((option) => option.value === post.category)} onChange={detectSelect} options={options} />
+                            </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="title" value="Title" />
+                                </div>
+                                <TextInput name="title" value={post.title} onChange={detectChange} className=" text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-red-600 focus:border-red-600 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-600 dark:focus:border-red-600" id="title" placeholder="Title" required />
+                            </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="description" value="Description" />
+                                </div>
+                                <TextInput name="description" value={post.description} onChange={detectChange} className=" text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-red-600 focus:border-red-600 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-600 dark:focus:border-red-600" id="description" placeholder='Optional' />
+                            </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="content" value="Content" />
+                                </div>
+                                <button type="button" class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+                                        <path fill="currentColor" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z" />
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z" />
+                                    </svg>
+                                    <span class="sr-only">Upload image</span>
+                                </button>
+                                <button type="button" class="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.408 7.5h.01m-6.876 0h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM4.6 11a5.5 5.5 0 0 0 10.81 0H4.6Z" />
+                                    </svg>
+                                    <span class="sr-only">Add emoji</span>
+                                </button>
+                                <textarea name="content" value={post.content} onChange={detectChange} style={textareaStyle} rows="1" class="block mx-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-red-600 focus:border-red-600 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-600 dark:focus:border-red-600" placeholder="Your message..."></textarea>
+                            </div>
+                            <div>
+                                <button type='submit' className="w-full mt-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                    <div className="flex items-center justify-center">
+                                        Post
+                                        <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </div>
                         </div>
-                    </div>
                     </form>
                 </Modal.Body>
             </Modal>
